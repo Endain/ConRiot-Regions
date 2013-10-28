2596b33ea9955f016b5a3f84b60da7a0c3becf13
Endain <endain.gaming@gmail.com>
2013-10-28 00:37:56 -0700

    Basic plugin framework added and various files configured.

---
+++ src/main/java/net/conriot/sona/regions/Regions.java
@@ -0,0 +1,15 @@
+package net.conriot.sona.regions;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Regions extends JavaPlugin {
+	@Override
+	public void onEnable() {
+		// TODO
+	}
+	
+	@Override
+	public void onDisable() {
+		// Nothing to do here
+	}
+}
